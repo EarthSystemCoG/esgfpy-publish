93ed06ba1a39999a86f3534824f299b3bad554f9
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2013-12-28 03:26:29 -0700

    Writing out dimensions name and size.

--- esgfpy/publish/parsers/netcdf_parser.py
+++ esgfpy/publish/parsers/netcdf_parser.py
@@ -17,10 +17,14 @@ class NetcdfMetadataFileParser(AbstractMetadataFileParser):
             # loop over global attributes
             for attname in nc.ncattrs():
                 self._addMetadata(metadata, attname, getattr(nc, attname) )
+                
+            # loop over dimensions
+            for key, dim in nc.dimensions.items():
+                self._addMetadata(metadata, 'dimension', "%s: %s" % (key, len(dim)) )
             
             # loop over variable attributes
             for key, variable in nc.variables.items():
-                if key.lower()!='longitude' and key.lower()!='latitude' and key.lower()!='altitude' and key.lower()!='time' and key.lower()!='level':
+                if not self._isCoordinate(key):
                     # IMPORTANT: the variable arrays must have the same number of entries
                     self._addMetadata(metadata, 'variable', key)
                     self._addMetadata(metadata, 'variable_long_name', getattr(variable, 'long_name', None) )
@@ -36,3 +40,7 @@ class NetcdfMetadataFileParser(AbstractMetadataFileParser):
                 pass
             
         return metadata
+    
+    def _isCoordinate(self, key):
+        return (key.lower()!='longitude' and key.lower()!='latitude' 
+                and key.lower()!='altitude' and key.lower()!='time' and key.lower()!='level')
