356ff028b2b332e873fa0b54701f0b9634dcf421
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2015-10-15 02:13:48 -0600

    Inserting 'shard' metadata fro CMAC records.

--- esgfpy/update/drivers/cmac.py
+++ esgfpy/update/drivers/cmac.py
@@ -12,8 +12,13 @@ logging.basicConfig(level=logging.DEBUG)
 
 SOLR_URL = 'http://esgf-node.jpl.nasa.gov:8982/solr'
 
-myDict = {'project:CMAC': {'index_node':['esgf-node.jpl.nasa.gov'] } }
+# 1) set index_node=esgf-node.jpl.nasa.gov for all datasets, files
+#myDict = {'project:CMAC': {'index_node':['esgf-node.jpl.nasa.gov'] } }
+#for core in ['datasets','files']:
+#    xmlDoc  = buildSolrXml(myDict, update='set', solr_url=SOLR_URL, solr_core=core)
+#    sendSolrXml(xmlDoc, solr_url=SOLR_URL, solr_core=core)
 
-for core in ['datasets','files']:
-    xmlDoc  = buildSolrXml(myDict, update='set', solr_url=SOLR_URL, solr_core=core)
-    sendSolrXml(xmlDoc, solr_url=SOLR_URL, solr_core=core)
+# 2) set shard=localhost:8982 for all datasets
+myDict = {'project:CMAC': {'shard':['localhost:8982'] } }
+xmlDoc  = buildSolrXml(myDict, update='set', solr_url=SOLR_URL, solr_core='datasets')
+sendSolrXml(xmlDoc, solr_url=SOLR_URL, solr_core='datasets')
