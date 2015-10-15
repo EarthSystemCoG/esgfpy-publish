6feeea6a99075ec108a32d5559ebe3f6ad7685a1
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2015-10-15 13:14:11 -0600

    Updating example script.

--- esgfpy/update/example.py
+++ esgfpy/update/example.py
@@ -3,12 +3,13 @@ from esgfpy.update.utils import sendSolrXml, buildSolrXml
 
 logging.basicConfig(level=logging.DEBUG)
 
+SOLR_URL = 'http://esgf-node.jpl.nasa.gov:8982/solr'
+
 # dictionary containing fields to be "set" (i.e. will override fields with the same name)
-myDict = {'id:test.test.v1.testData.nc|esgf-dev.jpl.nasa.gov': 
-          {'xlink':['http://esg-datanode.jpl.nasa.gov/thredds/fileServer/esg_dataroot/obs4MIPs/technotes/zosTechNote_AVISO_L4_199210-201012.pdf|AVISO Sea Surface Height Technical Note|summary']}}
-
+myDict = { 'project:GASS-YoTC-MIP': {'shard':['localhost:8989'] } }
+          
 # start XML document containing all update instructions
-xmlDoc  = buildSolrXml(myDict, update='set', solr_url='http://esgf-dev.jpl.nasa.gov:8984/solr', solr_core='files')
+xmlDoc  = buildSolrXml(myDict, update='set', solr_url=SOLR_URL, solr_core='datasets')
 
-sendSolrXml(xmlDoc, solr_url='http://esgf-dev.jpl.nasa.gov:8984/solr', solr_core='files')
+sendSolrXml(xmlDoc, solr_url=SOLR_URL, solr_core='datasets')
 
