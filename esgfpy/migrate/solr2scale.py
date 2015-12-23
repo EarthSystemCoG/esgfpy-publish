0d5d1d693f18eb7c6e7285b806f5b7748ce190d5
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2015-12-23 04:59:02 -0700

    Upgrading the solr scaling script.

--- esgfpy/migrate/solr2scale.py
+++ esgfpy/migrate/solr2scale.py
@@ -11,15 +11,23 @@ if __name__ == '__main__':
     sourceSolrUrl = "http://pcmdi9.llnl.gov/solr"
     targetSolrUrl = "http://localhost:8983/solr"
     core = "datasets"
-    replace = None
+    #replace = None
     #replace = "pcmdi9.llnl.gov:esgf-node.jpl.nasa.gov"
     #replace = "pcmdi9.llnl.gov:others"
     
     # total number of records indexed = maxRecords * numIterations
-    maxRecords = 150    # maximum number of records per migration
-    numIterations = 10  # number of migrations
+    maxRecords = 20000    # maximum number of records per migration
+    #numIterations = 20  # number of migrations
     
-    for i in range(1, 1+numIterations):
+    for replace in [None, "pcmdi9.llnl.gov:esgf-node.jpl.nasa.gov", "pcmdi9.llnl.gov:others"]:
         
-        suffix = ".%s" % i
-        migrate(sourceSolrUrl, targetSolrUrl, core, maxRecords=maxRecords, suffix=suffix, replace=replace)
+        if replace == "pcmdi9.llnl.gov:others":
+            numIterations = 10
+        else:
+            numIterations = 20 
+        
+        for i in range(1, 1+numIterations):
+            
+            print "Executing iteration #: %s for replacement=%s" % (i, replace)
+            suffix = ".%s" % i
+            migrate(sourceSolrUrl, targetSolrUrl, core, maxRecords=maxRecords, suffix=suffix, replace=replace)
