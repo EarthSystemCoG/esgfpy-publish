9a4a6beadcfc1892cfaf18a68f5d3c293d5a6b78
Luca Cinquini <cinquini@LMC-038261.local>
2017-07-05 17:38:06 -0600

    Fixing the sup data script

--- esgfpy/update/drivers/obs4mips_supplementary_data.py
+++ esgfpy/update/drivers/obs4mips_supplementary_data.py
@@ -7,8 +7,9 @@ SOLR_URL = 'http://esgf-node.jpl.nasa.gov:8984/solr'
 
 # associate supplementary data to datasets
 # http://esgf-data.jpl.nasa.gov/thredds/fileServer/obs4MIPs/technotes/cltTechNote_MODIS_L3_C5_200003-201109.pdf
-myDict = {'id:obs4MIPs.NASA-JPL.TES.tro3.mon.v20110608|esgf-data.jpl.nasa.gov':
-            {'xlink':['https://esgf-data.jpl.nasa.gov/thredds/fileServer/obs4MIPs/supplementary_data/TES-SUPPLEMENTARY.zip|TES Supplementary Data|supdata']},
+myDict = {'id:obs4mips.NASA-JPL.TES.tro3.mon.v20110608|esgf-data.jpl.nasa.gov':
+            {'xlink':['http://esgf-data.jpl.nasa.gov/thredds/fileServer/obs4MIPs/technotes/tro3TechNote_TES_L3_tbd_200507-200912.pdf|TES Ozone Technical Note|technote',
+                      'https://esgf-data.jpl.nasa.gov/thredds/fileServer/obs4MIPs/supplementary_data/TES-SUPPLEMENTARY.zip|TES Supplementary Data|supdata']},
           }
 
 updateSolr(myDict, update='set', solr_url=SOLR_URL, solr_core='datasets')
