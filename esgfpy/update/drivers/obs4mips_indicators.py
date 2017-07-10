1e80564a3c3a8733064e231a70f565c42578f497
Luca Cinquini <cinquini@ps-128-149-80-40.jpl.nasa.gov>
2017-07-10 15:25:27 -0600

    Updating the obs4mios indicators facility.

--- esgfpy/update/drivers/obs4mips_indicators.py
+++ esgfpy/update/drivers/obs4mips_indicators.py
@@ -1,81 +1,25 @@
-# sample script that reads a configuration file for ESGF quality control flags
+# sample script that reads a JSON configuration file for ESGF quality control flags
 # and publishes the information to the Solr index
 
 import logging
 from esgfpy.update.utils import updateSolr
-import ConfigParser
-import sys
+from pprint import pprint
 import json
-
-# input: configuration file example:
-# [obs4MIPs.NASA-JPL.AIRS.hus.mon.v1|esgf-dev.jpl.nasa.gov]
-# obs4mips_indicators_1=green
-# obs4mips_indicators_2=green
-# obs4mips_indicators_3=green
-# obs4mips_indicators_4=green
-# obs4mips_indicators_5=yellow
-# obs4mips_indicators_6=light_gray
-
-# intermediate output: python dictionary used by this script:
-#myDict = {'id:obs4MIPs.NASA-JPL.AIRS.hus.mon.v1|esgf-dev.jpl.nasa.gov': 
-#            {'quality_control_flags':['obs4mips_indicators:1:green', 
-#                                      'obs4mips_indicators:2:green',
-#                                      'obs4mips_indicators:3:green',
-#                                      'obs4mips_indicators:4:green',
-#                                      'obs4mips_indicators:5:yellow',
-#                                      'obs4mips_indicators:6:light_gray']},
-#         }
-
-# final output: Solr metadata:
-#"quality_control_flags": [
-#          "obs4mips_indicators:1:green",
-#          "obs4mips_indicators:2:green",
-#          "obs4mips_indicators:3:green",
-#          "obs4mips_indicators:4:green",
-#          "obs4mips_indicators:5:yellow",
-#          "obs4mips_indicators:6:light_gray"
-#        ],
 
 logging.basicConfig(level=logging.DEBUG)
 
-SOLR_URL = 'http://esgf-dev.jpl.nasa.gov:8984/solr'
+SOLR_URL = 'http://esgf-node.jpl.nasa.gov:8984/solr'
 
-myDict = {'id:obs4MIPs.NASA-JPL.AIRS.hus.mon.v1|esgf-dev.jpl.nasa.gov': 
-            {'quality_control_flags':['obs4mips_indicators:1:green', 
-                                      'obs4mips_indicators:2:green',
-                                      'obs4mips_indicators:3:green',
-                                      'obs4mips_indicators:4:green',
-                                      'obs4mips_indicators:5:yellow',
-                                      'obs4mips_indicators:6:light_gray']},
-         }
+# read indicators from JSON file
+json_file = 'obs4mips_indicators_jpl.json' # FIXME: use GitHub URL
+with open(json_file) as data_file:    
+    json_data = json.load(data_file)
 
-# read indicators from configuration file
-config_file = 'obs4mips_indicators_jpl.cfg' # FIXME: use GitHub URL
-config = ConfigParser.ConfigParser()
-# must set following line explicitly to preserve the case of configuration keys
-config.optionxform = str 
-myDict = {}
-try:
-    config.read(config_file)
-    for dataset_id in config.sections():
-        print 'Dataset id=%s' % dataset_id
-        qcflags_list = []
-        for key, value in config.items(dataset_id):
-            print '\t%s = %s' % (key, value)
-            # split the key at the last '_' to extract the numer
-            i = key.rfind("_")
-            qcflag_name = key[:i]
-            qcflag_number = key[i+1:]
-            qcflags_list.append( "%s:%s:%s" % (qcflag_name, qcflag_number, value) )
-        myDict['id:%s' % dataset_id] = { 'quality_control_flags' : qcflags_list }
-except Exception as e:
-    print "ERROR reading configuration:"
-    print e
-    sys.exit(-1)
+pprint(json_data)
 
 # publish to Solr
-print "Publishing metadata: %s" % myDict
-jDict = json.dumps(myDict)
-print jDict
-updateSolr(myDict, update='set', solr_url=SOLR_URL, solr_core='datasets')
+#print "Publishing metadata: %s" % myDict
+#jDict = json.dumps(myDict)
+#print jDict
+updateSolr(json_data, update='set', solr_url=SOLR_URL, solr_core='datasets')
 
