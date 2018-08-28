e424b1feda128e4365c95e13474404ab2b6f77ce
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2018-08-28 04:20:46 -0600

    Improving the harvesting script

--- scripts/solr_harvest.sh
+++ scripts/solr_harvest.sh
@@ -2,7 +2,7 @@
 
 # Script to harvest all records from a remote Solr server to a local Solr server.
 # Example invocation:
-# ./solr_harvest.sh http://esgf-node.jpl.nasa.gov/solr http://localhost:8983/solr datasets files aggregations
+# ./solr_harvest.sh http://esgdata.gfdl.noaa.gov/solr http://localhost:8983/solr datasets files aggregations
 
 set -e
 
@@ -22,6 +22,19 @@ cd $PARENT_DIR
 # loop over collections
 for collection in $collections
 do
-  echo "Harvesting collection=${collection}"
-  python esgfpy/migrate/solr2solr4all.py ${solr_source_url} ${solr_target_url} --core ${collection}
+  #numTotal=`curl -s 'http://esgdata.gfdl.noaa.gov/solr/'${collection}'/select/?q=*:*&wt=json&rows=0' | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["response"]["numFound"]'`
+  url="${solr_source_url}/${collection}"'/select/?q=*:*&wt=json&rows=0'
+  echo $url
+  numTotal=`curl -s "$url" | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["response"]["numFound"]'`
+  echo "Harvesting collection=${collection} total number of records=$numTotal"
+  
+  start=0
+  maxRecords=10
+  while [ $start -lt $numTotal ]; do
+     echo "\tStarting record=$start max records=$maxRecords"
+     
+     start=$((start + maxRecords))
+  done
+
+  #python esgfpy/migrate/solr2solr4all.py ${solr_source_url} ${solr_target_url} --core ${collection}
 done
